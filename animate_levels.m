d8722807a52430bb6d80eacfb960eeab62d3108f
Ionescu Alexandru <alx.ionescu97@gmail.com>
2018-11-12 11:11:35 +0200

    Properly animating tank levels

--- animate_levels.m
+++ animate_levels.m
@@ -2,29 +2,10 @@ function [] = animate_levels(h_data, h1_data, h2_data, h3_data, h4_data)
 	figure('units','normalized','outerposition',[0 0 1 1])
 
 	sHandle1 = subplot(3, 2, 1);
-	xlim(sHandle1, [0 1]);
-	ylim(sHandle1, [0 0.01]);
-	title(sHandle1, 'Nivel h1');
-
 	sHandle2 = subplot(3, 2, 2);
-	xlim(sHandle2, [0 1]);
-	ylim(sHandle2, [0 0.01]);
-	title(sHandle2, 'Nivel h2');
-
 	sHandle3 = subplot(3, 2, 3);
-	xlim(sHandle3, [0 1]);
-	ylim(sHandle3, [0 0.01]);
-	title(sHandle3, 'Nivel h3');
-
 	sHandle4 = subplot(3, 2, 4);
-	xlim(sHandle4, [0 1]);
-	ylim(sHandle4, [0 0.01]);
-	title(sHandle4, 'Nivel h4');
-
 	sHandle5 = subplot(3, 2, 5);
-	xlim(sHandle4, [0 1]);
-	ylim(sHandle4, [0 1.2]);
-	title(sHandle4, 'Nivel h');
 
 	for i=1:length(h_data);
 		area(sHandle1, [0 1],[h1_data(i) h1_data(i)]);
@@ -32,6 +13,26 @@ function [] = animate_levels(h_data, h1_data, h2_data, h3_data, h4_data)
 		area(sHandle3, [0 1],[h3_data(i) h3_data(i)]);
 		area(sHandle4, [0 1],[h4_data(i) h4_data(i)]);
 		area(sHandle5, [0 1],[h_data(i) h_data(i)]);
+
+		xlim(sHandle1, [0 1]);
+		ylim(sHandle1, [0 0.02]);
+		title(sHandle1, 'Nivel h1');
+
+		xlim(sHandle2, [0 1]);
+		ylim(sHandle2, [0 0.02]);
+		title(sHandle2, 'Nivel h2');
+
+		xlim(sHandle3, [0 1]);
+		ylim(sHandle3, [0 0.02]);
+		title(sHandle3, 'Nivel h3');
+
+		xlim(sHandle4, [0 1]);
+		ylim(sHandle4, [0 0.02]);
+		title(sHandle4, 'Nivel h4');
+
+		xlim(sHandle5, [0 1]);
+		ylim(sHandle5, [0 1.2]);
+		title(sHandle5, 'Nivel h');
 
 		pause(.1);
 	end
