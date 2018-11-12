fd445212a7a510c0bdbeaff865ff455d4f0f374d
Ionescu Alexandru <alx.ionescu97@gmail.com>
2018-11-12 13:31:53 +0200

    Grafice subpunctul B

--- tema_comm.m
+++ tema_comm.m
@@ -1,32 +1,4 @@
 %% functionname: function description
-l1 = 2/4;
-l2 = 3/4;
-
-q = 1000;			% kg/m^3
-g = 9.8; 			% m/s^2
-
-A1 = 0.06;			% m^2
-A2 = 0.06;			% m^2
-A3 = 0.06;			% m^2
-A4 = 0.06;			% m^2
-AT = 0.1273;		% m^2
-
-cih1 = 0;
-cih2 = 0;
-cih3 = 0;
-cih4 = 0;
-cih  = 1;
-
-a1 = 1.31 / 10^4;	% m^2
-a2 = 1.51 / 10^4;	% m^2
-a3 = 9.27 / 10^5;	% m^2
-a4 = 8.82 / 10^5;	% m^2
-ac = 4.307 / 10^6;	% m^2
-
-step1 = 2; 			% V
-step2 = 1;			% V
-kp = 0.5 / 10^4;	% m^3/(s*V)
-
 load_system(model_name)
 res = sim(model_name,'StartTime','0','StopTime','500','FixedStep','0.2');
 close_system(model_name)
@@ -37,10 +9,13 @@ h2 = res.h2;
 h3 = res.h3;
 h4 = res.h4;
 
-animate_levels(h.Data, h1.Data, h2.Data, h3.Data, h4.Data);
+if (animation_enable == 1)
+	animate_levels(h.Data, h1.Data, h2.Data, h3.Data, h4.Data);
+else
+	fprintf("Animatia este dezactivata.\n");
+end
 
-
-figure(2)
+figure;
 
 subplot(2, 1, 1);
 plot(h2.Time, h2.Data)
---
+++ tema_comm_b.m
@@ -0,0 +1,66 @@
+%% functionname: function description
+load_system(model_name);
+
+figure(3);
+
+loop_len = length(arr_cih);
+
+out_arr2_data = zeros(100, loop_len);
+out_arr4_data = zeros(100, loop_len);
+out_arr_time  = zeros(100, loop_len);
+out_data_len  = [];
+
+for i=1:loop_len
+	cih  = arr_cih(i);
+	cih1 = arr_cih1(i);
+	cih2 = arr_cih2(i);
+	cih3 = arr_cih3(i);
+	cih4 = arr_cih4(i);
+
+	res = sim(model_name,'StartTime','0','StopTime','500','FixedStep','0.2');
+
+	h  = res.h;
+	h1 = res.h1;
+	h2 = res.h2;
+	h3 = res.h3;
+	h4 = res.h4;
+
+	out_data_len(end+1) = length(h.Time);
+	out_arr2_data(1:out_data_len(i), i) = h2.Data;
+	out_arr4_data(1:out_data_len(i), i) = h4.Data;
+	out_arr_time(1:out_data_len(i), i)  = h.Time;
+end
+
+sHandle1 = subplot(2, 1, 1);
+hold on;
+for i=1:loop_len
+	leg = sprintf("C = %.2f, C_1 = %.2f, C_2 = %.2f, C_3 = %.2f, C_4 = %.2f", arr_cih(i), arr_cih1(i), arr_cih2(i), arr_cih3(i), arr_cih4(i));
+	plot(sHandle1, out_arr_time(1:out_data_len(i), i), out_arr2_data(1:out_data_len(i), i), 'DisplayName', leg);
+end
+hold off;
+
+title(sHandle1, 'h2(t)');
+xlabel(sHandle1, 'Timp (s)')
+ylabel(sHandle1, 'Inaltime (m)');
+legend show;
+
+sHandle2 = subplot(2, 1, 2);
+hold on;
+for i=1:loop_len
+	leg = sprintf("C = %.2f, C_1 = %.2f, C_2 = %.2f, C_3 = %.2f, C_4 = %.2f", arr_cih(i), arr_cih1(i), arr_cih2(i), arr_cih3(i), arr_cih4(i));
+	plot(sHandle2, out_arr_time(1:out_data_len(i), i), out_arr4_data(1:out_data_len(i), i), 'DisplayName', leg);
+end
+hold off;
+
+title(sHandle2, 'h4(t)');
+xlabel(sHandle2, 'Timp (s)')
+ylabel(sHandle2, 'Inaltime (m)');
+legend show;
+
+if (animation_enable == 1)
+	animate_levels(h.Data, h1.Data, h2.Data, h3.Data, h4.Data);
+else
+	fprintf("Animatia este dezactivata.\n");
+end
+
+close_system(model_name);
